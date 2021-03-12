7f01abe4c8dbb77444a31b6001a0a74dd19f1b38
CRStromberg <Cstromberg86@gmail.com>
2021-03-11 17:13:55 -0700

    tests finished

--- hw1/city.py
+++ hw1/city.py
@@ -1,21 +1,27 @@
-class city:
+class City:
 
-    def __init__(self,size : int, name : str, population : int = 0) -> None:
-        self._size : int = size
+    def __init__(self,size : float, name : str, population : int = 0) -> None:
+        self._size : float = size
         self._name : str = name
         self._population : int = population
 
     def isNewCity(self) -> bool:
         return self.population == 0
 
+    @size.setter
+    def name(self, area: float) -> None:
+        if area < 0:
+            raise ValueError('Error, Not Enough Land!')
+        self._size = area
+
     @property 
     def size(self):
         return self._size
 
     def density(self) -> str:
-        if self._population < 100,000:
-            return "town"
-        elif self._population < 10,000,000:
+        if self._population < 100000:
+            return "Town"
+        elif self._population < 10000000:
             return "City"
         else: 
             return "MegaCity"
@@ -23,9 +29,3 @@ class city:
     @property
     def name(self) -> str:
         return self._name
-
-    @name.setter
-    def name(self,title : str) -> None:
-        if not title in ['Denver','Ouray','Fort Collins','SteamBoat']:
-            raise ValueError('Error, Invalid City Name!')
-        self._name = title
--- hw1/citytest.py
+++ hw1/citytest.py
@@ -1,1 +1,25 @@
 import unittest
+
+from city import City
+
+class CityTest(unittest.TestCase):
+    def testDefCity(self):
+        size : float = 0.86
+        name : str = "Ouray"
+        population : int = 1000
+
+        city : City = City(size, name, population)
+        self.assertEqual(city.population, population)
+        self.assertEqual(city.name, name)
+        self.assertEqual(city.size, size)
+
+    def cityType(self):
+        size : float = 0.86
+        name : str = "Ouray"
+        population : int = 1000
+
+        city : City = City(size, name, population)
+        self.assertEqual(city.density(), "Town")
+
+if __name__ == '__main__':
+    unittest.main()
