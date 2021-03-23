7553c6020561ae33259ad6fb4f167d8595c2ed38
CRStromberg <Cstromberg86@gmail.com>
2021-03-23 16:20:35 -0600

    Test done

--- hw3/test/citytest.js
+++ hw3/test/citytest.js
@@ -1,7 +1,23 @@
 
 const assert = require('assert');
 const chai = require('chai');
+const { isMainThread } = require('worker_threads');
 const { City }  = require('../city');
 
 const expect = chai.expect;
 const assertEq = assert.deepStrictEqual
+
+describe('City', function() {
+    describe('#constructor', function() {
+        it('can be made', function() {
+            const size = 0.86;
+            const name = "Ouray";
+            const population = 1000;
+            const city = new City(size, name, population);
+            assertEq(city.population, population);
+            assertEq(city.name, name);
+            assertEq(city.size, size);
+            assertEq(city.density(), "Town");
+        });
+    });
+});
