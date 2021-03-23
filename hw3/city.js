f7aad30651f455242bd9f7ba0920265e3d6cfe2e
CRStromberg <Cstromberg86@gmail.com>
2021-03-23 16:13:18 -0600

    main done

--- hw3/city.js
+++ hw3/city.js
@@ -1,3 +1,42 @@
 class City {
-    
+    constructor(size, name, population) {
+        this._size = size;
+        this._name = name;
+        this._population = population;
+    }
+
+    isNewCity() {
+        return this._population == 0;
+    }
+
+    density() {
+        if(this._population < 100000) {
+            return "Town";
+        } else if(this._population < 10000000) {
+            return "City";
+        } else {
+            return "MegaCity";
+        }
+    }
+
+    get population() {
+        return this._population;
+    }
+
+    get size() {
+        return this._size;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set landSize(area) {
+    if(area <= 0) {
+        throw Error('Error, Not Enough Land!');
+    }
+    this._size = area;
 }
+}
+
+module.exports = {'City' : City }
