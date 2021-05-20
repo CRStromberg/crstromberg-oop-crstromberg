ea462164b2db2bc89a27add2a4ec73e8d40563a5
CRStromberg <Cstromberg86@gmail.com>
2021-05-20 10:10:41 -0600

    VehicleBuilder Class added

--- Final/builder.js
+++ Final/builder.js
@@ -19,3 +19,35 @@ class Vehicle {
         this.name = name;
     }
 }
+
+class VehicleBuilder {
+    constructor(name) {
+        this.name = new Vehicle(name);
+    }
+
+    setType(vType) {
+        if(vType == "Truck" || vType == "Car" || vType == "Van") {
+            this.name.vType = vType;
+        } else throw Error ("Error, incorrect vehicle type.")
+
+        return this
+    }
+
+
+    setEngine(engine) {
+        
+    this.name.engine = new Engine(engine.eSize, engine.eMileage);
+
+    return this
+    }
+    
+    build() {
+        return this.name;
+    }
+}
+
+module.exports = {'VehicleBuilder' : VehicleBuilder }
+
+//let engineTest = {eSize:4.5, eMileage: 18};
+//let test = new VehicleBuilder('Chris').setEngine(engineTest).build();
+//console.log(test.engine.engineMileage)
